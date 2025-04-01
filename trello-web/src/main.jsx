1c1d1b6aa042a26434f60c770cbe7aa971735388
htthangcse <hatienthangk22cse@gmail.com>
2025-04-01 16:17:07 +0700

    fix

--- trello-web/src/main.jsx
+++ trello-web/src/main.jsx
@@ -1,15 +1,34 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import CssBaseline from '@mui/material/CssBaseline'
+// import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
+
+// import theme from './theme.js'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <CssVarsProvider theme={theme} defaultMode="dark" colorSchemeSelector="body">
+//       <CssBaseline />
+//       <App />
+//     </CssVarsProvider>
+//   </StrictMode>,
+// )
+
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@mui/material/styles'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme} defaultMode="light" colorSchemeSelector="body">
       <CssBaseline />
       <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </StrictMode>,
 )
--- trello-web/src/theme.js
+++ trello-web/src/theme.js
@@ -1,21 +1,56 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+// import { extendTheme } from '@mui/material/styles'
 
-// Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#556cd6'
+
+
+// // Create a theme instance.
+// const theme = extendTheme ({
+//   colorSchemes: {
+//     light:  {
+//       palette: {
+//         primary: {
+//           // main: '#ff5252',
+//         },
+//       },
+//     },
+//     dark:  {
+//       palette: {
+//         primary: {
+//           // main: '#000',
+//         },
+//       },
+//     },
+//   }
+// });
+
+// export default theme
+
+
+import { extendTheme } from '@mui/material/styles'
+import { red, blue } from '@mui/material/colors'
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: blue[500],
+        },
+        secondary: {
+          main: red[500],
+        },
+      },
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
+    dark: {
+      palette: {
+        primary: {
+          main: blue[300],
+        },
+        secondary: {
+          main: red[300],
+        },
+      },
     },
   },
-});
+})
 
 export default theme
