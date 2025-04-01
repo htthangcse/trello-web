b05f12f8d54e0f81a84a08c636acaf00bc38cf1b
htthangcse <hatienthangk22cse@gmail.com>
2025-04-01 16:17:07 +0700

    fix

--- trello-web/src/App.jsx
+++ trello-web/src/App.jsx
@@ -1,15 +1,53 @@
-import Button from '@mui/material/Button'
-import HomeIcon from '@mui/icons-material/Home'
+// import Button from '@mui/material/Button'
+// import HomeIcon from '@mui/icons-material/Home'
 
-function App() {
+// import { useColorScheme } from '@mui/material/styles'
+
+// function ModeToggle () {
+//   const { mode, setMode } = useColorScheme()
+//   return (
+//     <Button
+//       onClick = {() => {
+//           setMode(mode === 'light' ? 'dark' : 'light')
+//       }}>
+//         {mode === 'light' ? 'Turn dark':  'Turn light'}
+//     </Button>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <>
+//       <ModeToggle/>
+//       <Button variant="text">Text</Button>
+//       <Button variant="contained">Contained</Button>
+//       <Button variant="outlined">Outlined</Button>
+//       <HomeIcon/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import { useColorScheme } from '@mui/material/styles'
+import Button from '@mui/material/Button'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+
   return (
-    <>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <HomeIcon/>
-    </>
+    <Button onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}>
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <>
+      <ModeToggle />
+      <Button variant="contained">Contained</Button>
+    </>
+  )
+}
