8c03f99d58189e11fff730afb3f57f12561919e5
htthangcse <hatienthangk22cse@gmail.com>
2025-04-01 16:45:02 +0700

    add basic layout

--- trello-web/src/App.jsx
+++ trello-web/src/App.jsx
@@ -32,6 +32,8 @@
 
 import { useColorScheme } from '@mui/material/styles'
 import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
@@ -45,9 +47,34 @@ function ModeToggle() {
 
 export default function App() {
   return (
-    <>
-      <ModeToggle />
-      <Button variant="contained">Contained</Button>
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh',  }}>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+        <ModeToggle />
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width: '100%',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+        Board Content
+      </Box>
+    </Container>
   )
 }
