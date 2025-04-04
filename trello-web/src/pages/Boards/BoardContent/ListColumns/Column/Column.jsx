1e477cc1d0d0744f4d0bdae5b99259f40d667a4a
htthangcse <hatienthangk22cse@gmail.com>
2025-04-04 15:34:53 +0700

    add drag drop columns

--- trello-web/src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ trello-web/src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -19,24 +19,47 @@ import DragHandleIcon from '@mui/icons-material/DragHandle'
 import ListCards from './ListCards/ListCards'
 
 import { mapOrder } from '~/utils/sorts'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 function Column({ column }) {
+  /*
+  The items are stretched because you're using CSS.Transform.toString(), 
+  use CSS.Translate.toString() if you don't want to have the scale 
+  transformation applied.
+  link github: https://github.com/clauderic/dnd-kit/issues/117
+   */
+  const { attributes, listeners, setNodeRef, transform, transition, } = useSortable({
+    id: column._id,
+    data: {...column}
+  })
+  const dndKitColumnStyles  = {
+    //touchAction: 'none', // danh cho sensor default dạng PointerSensor
+    transform: CSS.Translate.toString(transform),
+    transition,
+  }
+
+  // drop down menu
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => { setAnchorEl(event.currentTarget); };
   const handleClose = () => { setAnchorEl(null); };
 
   const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id' )
-
   return (
-    <Box sx={{ 
-      minWidth: '300px',
-      maxWidth: '300px',
-      bgcolor: '#ebecf0',
-      ml: 2,
-      borderRadius: '6px',
-      height: 'fit-content',
-      maxHeight: (theme) =>  `calc(${theme.trello.boadrContentHeight} - ${theme.spacing(5)} )`
+    <Box 
+      ref={setNodeRef}
+      style={dndKitColumnStyles}
+      {...attributes}
+      {...listeners}
+      sx={{ 
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: '#ebecf0',
+        ml: 2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        maxHeight: (theme) =>  `calc(${theme.trello.boadrContentHeight} - ${theme.spacing(5)} )`
     }}>
       {/* box header */}
       <Box sx={{
--- trello-web/src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ trello-web/src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -2,48 +2,56 @@ import Box from '@mui/material/Box'
 import Column from './Column/Column'
 import Button from '@mui/material/Button'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
+import {SortableContext, horizontalListSortingStrategy} from '@dnd-kit/sortable'
 
 function ListColumns({ columns }) {
-
+  /*
+  The <SortableContext> component requires that you pass it the sorted array of
+  the unique identifiers associated to each sortable item via the items prop. 
+  This array should look like ["1", "2", "3"], not [{id: "1"}, {id: "2}, {id: "3}].
+  link github: https://github.com/clauderic/dnd-kit/issues/183#issuecomment-812569512
+  */
   return (
-    <Box sx={{
-      bgcolor: 'inherit',
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      overflowX: 'auto',
-      ovevrflowY: 'hidden',
-      '&::-webkit-scrollbar-track':{ m: 2,}
-    }}>
-      {/* khi co 1 columns thi nhu nay */}
-      {columns?.map((column) => <Column key={column._id} column={column}/>)} 
-      {/* {columns?.map((column) => {
-        return (<Column key={column._id} column={column}/>)
-      })} */}
-      
-      {/* add new column */}
+    <SortableContext items={columns?.map(c => c._id)} strategy={horizontalListSortingStrategy}>
       <Box sx={{
-        minWidth: '200px',
-        maxWidth: '200px',
-        mx: 2,
-        borderRadius: '6px',
-        height: 'fit-content',
-        bgcolor: '#ffffff3d'
+        bgcolor: 'inherit',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        overflowX: 'auto',
+        ovevrflowY: 'hidden',
+        '&::-webkit-scrollbar-track':{ m: 2,}
       }}>
-        <Button
-        startIcon={<NoteAddIcon/>}
-        sx={{
-          color: 'white',
-          width: '100%',
-          justifyContent: 'flex-start',
-          pl: 2.5,
-          py: 1,
-        }}
-        >
-          Add new column
-        </Button>
+        {/* khi co 1 columns thi nhu nay */}
+        {columns?.map((column) => <Column key={column._id} column={column}/>)} 
+        {/* {columns?.map((column) => {
+          return (<Column key={column._id} column={column}/>)
+        })} */}
+        
+        {/* add new column */}
+        <Box sx={{
+          minWidth: '200px',
+          maxWidth: '200px',
+          mx: 2,
+          borderRadius: '6px',
+          height: 'fit-content',
+          bgcolor: '#ffffff3d'
+        }}>
+          <Button
+          startIcon={<NoteAddIcon/>}
+          sx={{
+            color: 'white',
+            width: '100%',
+            justifyContent: 'flex-start',
+            pl: 2.5,
+            py: 1,
+          }}
+          >
+            Add new column
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   )
 }
 
