42eb4963efd1bb1c07f24eb336cc077849ef65ee
htthangcse <hatienthangk22cse@gmail.com>
2025-05-03 23:34:12 +0700

    react toastify

--- trello-web/src/main.jsx
+++ trello-web/src/main.jsx
@@ -5,11 +5,16 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from '~/theme.js'
 
+// Cấu hình react-toastify
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     <CssVarsProvider theme={theme} defaultMode="light" colorSchemeSelector="body">
       <CssBaseline />
       <App />
+      <ToastContainer theme="colored" position="bottom-left"/>
     </CssVarsProvider>
   // {/* </StrictMode>, */}
 )
