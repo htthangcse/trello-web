bfb18f8a09067df44fe3b7fb5fc4bd2a9631b5f2
htthangcse <hatienthangk22cse@gmail.com>
2025-05-18 11:19:41 +0700

    66-69 adđ new column and card

--- trello-web/src/apis/index.js
+++ trello-web/src/apis/index.js
@@ -3,9 +3,22 @@ import { API_ROOT } from '~/utils/constant'
 
 // Chúng ta sẽ try catch các lỗi chung bằng Interceptors sau này Advance
 
+// Boards
 export const fetchBoardDetailsAPI = async (boardId) => {
   const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`) // ``: string literal
   // Lưu ý: axios sẽ trả về kết quả qua property của nó là data
   console.log('api: ',response.data)
+  return response.data
+}
+
+// Columns
+export const createNewColumnAPI = async (newColumnData) => {
+  const response = await axios.post(`${API_ROOT}/v1/columns`, newColumnData)
+  return response.data
+}
+
+// Cards
+export const createNewCardAPI = async (newCardData) => {
+  const response = await axios.post(`${API_ROOT}/v1/cards`, newCardData)
   return response.data
 }
--- trello-web/src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ trello-web/src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -26,7 +26,7 @@ import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import { Opacity } from '@mui/icons-material'
 
-function Column({ column }) {
+function Column({ column, createNewCard }) {
   /*
   The items are stretched because you're using CSS.Transform.toString(), 
   use CSS.Translate.toString() if you don't want to have the scale 
@@ -61,13 +61,25 @@ function Column({ column }) {
   const toggleOpenNewCardForm = () => setopenNewCardForm(!openNewCardForm)
 
   const [newCardTitle, setNewCardTitle] = useState('')
-  const addNewCard = () => {
+  const addNewCard = async () => {
     if(!newCardTitle) {
       toast.error('Please enter Card Title!', { position: "bottom-right"})
       return
     }
-    // console.log(newCardTitle)
-    // Gọi API ở đây...
+    
+    // Tạo dữ liệu Card để gọi API
+    const newCardData = {
+      title: newCardTitle,
+      columnId: column?._id,
+    }
+     /**
+     * Gọi lên props function createNewCard nằm ở component cha cao nhất (boards/_id.jsx)
+     * Lưu ý: Về sau ở học phần MERN STACK Advance học trực tiếp thì chúng ta sẽ đưa dữ liệu board ra ngoài Redux Global Store
+     * Thì lúc này chúng ta có thể gọi API ở đây là xong thay vì phải lần lượt gọi ngược lên những
+     * component cha phía bên trên. (Đối với component con nằm càng sâu thì càng khổ)
+     * Với việc sử dụng Redux như vậy thì code sẽ Clean chuẩn chỉnh hơn rất nhiều
+     */
+    await createNewCard(newCardData)
 
     // Đóng trạng thái thêm Card mới & clear input
     toggleOpenNewCardForm()
